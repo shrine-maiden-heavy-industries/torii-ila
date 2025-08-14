ff3bc716b82f6de09672b4745ff7c5e2205e1641
Aki Van Ness <aki@lethalbit.net>
2025-08-14 14:45:06 -0700

    examples: Removed uneeded trig signal

--- examples/bitsy_usb_ila.py
+++ examples/bitsy_usb_ila.py
@@ -144,7 +144,6 @@ class Top(Elaboratable):
 
 		wiggle = Signal()
 		woggle = Signal()
-		trig  = Signal()
 
 		# Add some "Private" signals to the ILA
 		self.ila.append_signals([wiggle, woggle])
@@ -177,8 +176,7 @@ class Top(Elaboratable):
 		with m.Else():
 			m.d.sync += [ self.timer.eq(self.timer - 1), ]
 
-		with m.If(self.other[7] & ~trig):
-			m.d.sync += [ trig.eq(1), ]
+		with m.If(self.other[7]):
 			m.d.comb += [ self.ila.trigger.eq(1) ]
 
 		# Glue for the PLL and LEDs
